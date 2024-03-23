aade3bda390e459b79602b471777ee0a93dfdae9
Ryan Murzyn <orian3737@gmail.com>
2024-03-23 12:47:38 -0400

    crazy hard tests to pass

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Write your solution in this file!
+  const employee = { 
+    name: 'Ryan',
+     streetAddress:'11 red maple ct' 
+};
+
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmployee = Object.assign({}, employee);
+    newEmployee[key] = value;
+    return newEmployee;
+  }
+  
+ 
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+  
+
+  function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = Object.assign({}, employee);
+    delete newEmployee[key];
+    return newEmployee;
+  }
+  
+  
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
+  
